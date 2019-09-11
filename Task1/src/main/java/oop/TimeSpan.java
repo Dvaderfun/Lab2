337965963252344445550c980dca2f7aeb0a95bc
MykolaHodovychenko <nick.godov@gmail.com>
2019-09-11 07:18:10 +0300

    Task1 target class modified

--- Task1/src/main/java/oop/TimeSpan.java
+++ Task1/src/main/java/oop/TimeSpan.java
@@ -2,50 +2,30 @@ package oop;
 
 public class TimeSpan {
 
-    private int minutes;
-
-    public TimeSpan(int hours, int minutes) {
-        this.add(hours, minutes);
-    }
+    public TimeSpan(int hours, int minutes) {}
 
     public int getHours() {
-        return this.minutes / 60;
+        return 0;
     }
 
     public int getMinutes() {
-        return this.minutes - (getHours() * 60);
-    }
-
-    public void add(int hours, int minutes) {
-        if (hours > -1 && minutes > -1 && minutes < 60)
-            this.minutes = hours * 60 + minutes;
-    }
-
-    public void addTimeSpan(TimeSpan span) {
-        this.minutes += span.getTotalMinutes();
+        return 0;
     }
 
     public double getTotalHours() {
-        double result = 0;
-
-        int minutes_left = this.minutes - getHours() * 60;
-        result = getHours() + (minutes_left / 60.0);
-
-        return result;
+        return 0;
     }
 
     public int getTotalMinutes() {
-        return this.minutes;
+        return 0;
     }
 
-    public void subtract(TimeSpan span) {
-        if (this.minutes >= span.getTotalMinutes())
-            this.minutes = this.minutes - span.getTotalMinutes();
-    }
+    public void add(int hours, int minutes) {}
 
-    public void scale(int factor) {
-        if (factor >= 0)
-            this.minutes = this.minutes * factor;
-    }
+    public void addTimeSpan(TimeSpan span) {}
+
+    public void subtract(TimeSpan span) {}
+
+    public void scale(int factor) {}
 
 }
