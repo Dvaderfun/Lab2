10faf62eba3172584e3f688ee30d9166235c5a78
MykolaHodovychenko <nick.godov@gmail.com>
2019-09-11 07:53:15 +0300

    Task2 target class modified

--- Task2/src/main/java/oop/BankAccount.java
+++ Task2/src/main/java/oop/BankAccount.java
@@ -4,31 +4,13 @@ public class BankAccount {
 
     private String name;
     private double balance;
-    private double transactionFee;
-
-    public void setTransactionFee(double transactionFee) {
-        this.transactionFee = transactionFee;
-    }
 
     public void deposit(double amount) {
         balance = balance + amount;
     }
 
     public boolean withdraw(double amount) {
-        if (amount + transactionFee > balance)
-            return false;
-
-        balance = balance - (amount + transactionFee);
-        return true;
-    }
-
-    public boolean transfer(BankAccount receiver, double amount) {
-        if (amount < 0)
-            return false;
-
-        if (withdraw(amount))
-            receiver.deposit(amount);
-
+        balance = balance - amount;
         return true;
     }
 }
