c218e057934e0609baa576421af9948762517c6b
Darth Vader <dvaderfun@gmail.com>
2019-10-03 00:32:53 +0300

    BankAccount

--- Task2/src/main/java/oop/BankAccount.java
+++ Task2/src/main/java/oop/BankAccount.java
@@ -4,25 +4,37 @@ public class BankAccount {
 
     private String name;
     private double balance;
+    private double transactionFee;
 
     public void deposit(double amount) {
-        balance = balance + amount;
+        if (amount > 0) {
+            balance = balance + amount;
+        }
     }
 
     public double getBalance() {
-        return this.balance;
+        return balance;
     }
 
     public boolean withdraw(double amount) {
-        balance = balance - amount;
-        return true;
+        if (amount > 0 && amount + transactionFee <= balance) {
+            balance = balance - (amount + transactionFee);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public void setTransactionFee(double fee) {
-
+        transactionFee = fee;
     }
 
-    public void transfer(BankAccount other, double amount) {
-
+    public boolean transfer(BankAccount receiver, double amount) {
+        if (withdraw(amount)) {
+            receiver.balance = receiver.balance + amount;
+            return true;
+        } else {
+            return false;
+        }
     }
 }
